603ebd0833b7412128cf548a87032c1c9b52ffdd
Sam Slover <sjs663@nyu.edu>
2013-03-08 12:47:03 -0500

    working

---
+++ models/clothes.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// define a new schema
+var ClothingSchema = new Schema({
+    //_id : Number,
+	//user_id : Number,
+	photo : String,
+	name : String,
+	type : String,
+	caption : String,
+	brand : String,
+    lastupdated : { type: Date, default: Date.now }
+});
+
+// export 'Clothing' model
+module.exports = mongoose.model('Clothing',ClothingSchema);
